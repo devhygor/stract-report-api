167cdba6aa7848f4b196e1077885181c9a086ae3
Hygor Rocha <hygor.rocha@resale.com.br>
2025-02-15 18:39:28 -0300

    add tests

---
+++ tests/test_app.py
@@ -0,0 +1,40 @@
+import pytest
+from app import app
+
+@pytest.fixture
+def client():
+    """Fixture para configurar o client de testes do Flask"""
+    with app.test_client() as client:
+        yield client
+
+def test_home(client):
+    """Testa o endpoint raiz"""
+    response = client.get('/')
+    assert response.status_code == 200
+    assert b"<h1>Home</h1>" in response.data
+
+def test_plataforma(client):
+    response = client.get('/meta_ads')
+    assert response.status_code == 200
+    assert b"de meta_ads" in response.data
+
+def test_plataforma_resumo(client):
+    """Testa o endpoint de resumo de uma plataforma"""
+    response = client.get('/meta_ads/resumo')
+    assert response.status_code == 200
+    assert b"Resumo de meta_ads" in response.data  # Ajuste conforme o conteúdo esperado
+
+def test_geral(client):
+    """Testa o endpoint geral"""
+    response = client.get('/geral')
+    assert response.status_code == 200
+    assert b"Geral" in response.data  # Ajuste conforme o conteúdo esperado
+
+def test_geral_resumo(client):
+    """Testa o endpoint de resumo geral"""
+    response = client.get('/geral/resumo')
+    assert response.status_code == 200
+    assert b"Resumo Geral" in response.data  # Ajuste conforme o conteúdo esperado
+
+if __name__ == "__main__":
+    pytest.main()
